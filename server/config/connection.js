dbb8aa9a5758a41a808278648d644f7afb6b0b39
Jordan Pace <jordanpace95@gmail.com>
2023-05-27 20:55:56 -0600

    messing with connections file

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,21 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 console.log(process.env.MONGODB_URI);
 
-// preparing for heroku deployment
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/googlebooks');
+// dotenv process for DB accsess
+const mongodburl =
+  process.env.MONGODB_URI ||
+  `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster1.p3vpljo.mongodb.net/?retryWrites=true&w=majority`;
+// Connect to MongoDB
+mongoose
+  .connect(mongodburl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 module.exports = mongoose.connection;
